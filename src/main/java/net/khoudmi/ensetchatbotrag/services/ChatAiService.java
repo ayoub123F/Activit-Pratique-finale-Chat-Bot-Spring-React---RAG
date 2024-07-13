675c9e16e4f69033c455773ea4a6304e2719e611
chahou <chahouayoub1@gmail.com>
2024-07-13 22:39:54 +0100

    master

---
+++ src/main/java/net/khoudmi/ensetchatbotrag/services/ChatAiService.java
@@ -0,0 +1,23 @@
+package net.khoudmi.ensetchatbotrag.services;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.hilla.BrowserCallable;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@BrowserCallable
+@AnonymousAllowed
+public class ChatAiService {
+    private ChatClient chatClient;
+
+    public ChatAiService(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    public String ragChat(String question){
+        return chatClient.prompt()
+                .user(question)
+                .call()
+                .content();
+    }
+}
---
+++ src/main/java/net/khoudmi/ensetchatbotrag/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package net.khoudmi.ensetchatbotrag.services;
+
+import org.springframework.lang.NonNullApi;
